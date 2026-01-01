719855356c034796caa83ad78c17ecafa0cd0e36
Joey Hendrickson <JoeyHendrickson@me.com>
2026-01-01 12:31:47 -0500

    fix: Add Supabase domains to CSP connect-src to allow sign out

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,7 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline' https://*.plaid.com https://*.plaid.io",
               "img-src 'self' data: https: blob:",
               "font-src 'self' data: https:",
-              "connect-src 'self' https://*.plaid.com https://*.plaid.io https://www.google.com https://www.gstatic.com https://*.googleapis.com wss://www.walletlink.org wss://mainnet.infura.io https://*.seondnsresolve.com data:",
+              "connect-src 'self' https://*.supabase.co https://*.supabase.in https://*.plaid.com https://*.plaid.io https://www.google.com https://www.gstatic.com https://*.googleapis.com wss://www.walletlink.org wss://mainnet.infura.io https://*.seondnsresolve.com data:",
               "frame-src 'self' https://*.plaid.com https://*.plaid.io",
               "frame-ancestors 'self'",
               "base-uri 'self'",
